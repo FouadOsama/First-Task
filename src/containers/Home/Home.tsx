2334cc2c3be7b07c8ada05b60a70b928ad51f95b
Fouad Osama <fouad.osama.shouman@gmail.com>
2024-08-27 12:37:48 +0300

    initial commit

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Home = (): JSX.Element => {
 	return (
+
+<div className="bg-inputBg h-screen flex justify-center items-center p-10 m-0">
+
 			<h1 className="text-3xl font-bold underline text-primary">Home Page</h1>
+			</div>
 		)
 }
 
